c7acfe8b511b4a98b297893f083567a0f85fb0bf
Gabriel Oliveira Quaresma <gabriel.oliveira.quaresma@gmail.com>
2024-03-08 20:30:14 -0300

    start commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text-red-900 font-bold bg-black/50">Login</h1>
+      <body>OLA GNET</body>
+      <input type="text" placeholder="OI" />
+    </div>
+  );
+}
+
+export default App;
