cc9f4b9fe7d2517cf554fa7f0f91a0402904110b
Gabriel Oliveira Quaresma <gabriel.oliveira.quaresma@gmail.com>
2024-03-08 21:53:53 -0300

    change bg

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import bg from "./assets/bg.png";
+
 function App() {
   return (
-    <div>
-      <h1 className="text-red-900 font-bold bg-black/50">Login</h1>
-      <body>OLA GNET</body>
-      <input type="text" placeholder="OI" />
+    <div className="flex">
+      <img src={bg} className="w-screen h-screen fixed z-0" />
+      <div className="w-screen h-screen bg-black/10 fixed z-0" />
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10">
+        <h1 className="text-9xl text-white">Hello, World!</h1>
+      </div>
     </div>
   );
 }
