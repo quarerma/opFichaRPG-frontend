8343ab8042bda1d943f1f0e5770956027c0401a7
Gabriel Oliveira Quaresma <gabriel.oliveira.quaresma@gmail.com>
2024-03-09 00:56:39 -0300

    login page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "login-gray": "#302F2F",
+        "black-filter": "#282020",
+        "red-bordo": "#4C0101",
+        "input-gray": "#737373",
+      },
+      fontFamily: {
+        oswald: ["Oswald", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
