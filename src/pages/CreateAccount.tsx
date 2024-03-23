e5ad8b7c91046a5aad811ef9d2a1bf0fff6acfab
Gabriel Oliveira Quaresma <gabriel.oliveira.quaresma@gmail.com>
2024-03-23 17:30:29 -0300

    cadastro

--- src/pages/CreateAccount.tsx
+++ src/pages/CreateAccount.tsx
@@ -3,9 +3,10 @@ import bg from "../assets/bg.png";
 import { GiBleedingEye } from "react-icons/gi";
 import { GiEyelashes } from "react-icons/gi";
 import { z } from "zod";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import axios, { AxiosError } from "axios";
 import { BASE_URL } from "../env";
+import { IoReturnUpBack } from "react-icons/io5";
 
 type CreateAccountProps = {
   username: string;
@@ -121,7 +122,6 @@ export const CreateAccount = () => {
           const status = axiosError.response.status;
           const data = axiosError.response.data;
           if (status === 409) {
-            console.log("entrou");
             setError(data.message);
           }
         }
@@ -137,14 +137,22 @@ export const CreateAccount = () => {
   };
 
   return (
-    <div className="h-screen w-screen flex flex-col">
+    <div className="h-screen w-screen flex flex-col text-white font-bold font-oswald">
       <div className=" z-10 my-auto justify-center ">
-        <div className=" flex flex-col items-center justify-center text-white font-bold font-oswald">
+        <div className=" flex flex-col items-center justify-center ">
           <div className=" gap-y-3  w-[380px] max-sm:w-[300px] h-fit py-5 rounded-xl bg-login-gray shadow-2xl px-8 max-sm:px-5">
             <div className="w-full h-full items-center justify-center flex flex-col">
-              <h1 className=" text-white text-center w-full text-4xl mb-5 ">
-                CADASTRE-SE
-              </h1>
+              <div className="flex w-full">
+                <Link to="/login" title="Voltar">
+                  <IoReturnUpBack
+                    size={40}
+                    className="font-extrabold absolute hover:scale-110 duration-300"
+                  />
+                </Link>
+                <h1 className=" text-white text-center w-full text-4xl mb-5 ">
+                  CADASTRE-SE
+                </h1>
+              </div>
               <form
                 onSubmit={onSubmit}
                 className="flex flex-col gap-y-3 w-full items-center"
